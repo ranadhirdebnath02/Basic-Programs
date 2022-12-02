7283d209b15a41d9c9882b388c7c1b091ebf0688
Ranadhir Debnath <ranadhirdebnath02@gmail.com>
2022-12-02 22:59:58 +0530

    java session 1 assignment program complete

---
+++ Java/Session-1/AssignmentProgram1.java
@@ -0,0 +1,9 @@
+/**
+ * Write a program that displays the text “Welcome To JAVA. Its great.”
+ */
+
+public class AssignmentProgram1 {
+  public static void main(String[] args) {
+    System.out.println("Welcome To JAVA. Its great.");
+  }
+}
---
+++ Java/Session-1/AssignmentProgram2.java
@@ -0,0 +1,13 @@
+/**
+ * Take two integer variables x an d y, where x=10 and y=20. Write a program to
+ * display the sum of these two variables.
+ */
+
+public class AssignmentProgram2 {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 20;
+    int sum = x + y;
+    System.out.println("The sum is " + sum);
+  }
+}
